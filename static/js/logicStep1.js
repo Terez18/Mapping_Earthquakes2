9c90a57f326a76cf77a3766fa8d3a8ac5c92bf47
Terez18 <tamarperez@comcast.net>
2021-04-23 14:47:23 -0700

    deleted portion of logicstep1 that had code from previous branch

--- static/js/logicStep1.js
+++ static/js/logicStep1.js
@@ -38,21 +38,3 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
 });
 
 
-let myStyle = {
-    color: "blue",
-    fillColor: "yellow",
-    weight: 1
-
-}
-
-// Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function (data) {
-    console.log(data);
-    // Creating a GeoJSON layer with the retrieved data.
-    L.geoJson(data, {
-        style: myStyle,
-        onEachFeature: function (feature, layer) {
-            layer.bindPopup("<h3> Neighborhood: " + feature.properties.AREA_NAME + "</h3>");
-        }
-    }).addTo(map);
-});
